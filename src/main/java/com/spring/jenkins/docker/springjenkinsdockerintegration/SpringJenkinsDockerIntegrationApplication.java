55df302f25b7fdbaea618be0ae2348961c02b404
Feven <feven.h.mamo@gmail.com>
2020-05-05 15:02:36 -0500

    first commit

---
+++ src/main/java/com/spring/jenkins/docker/springjenkinsdockerintegration/SpringJenkinsDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.docker.springjenkinsdockerintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDockerIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsDockerIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/jenkins/docker/springjenkinsdockerintegration/SpringJenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.docker.springjenkinsdockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
