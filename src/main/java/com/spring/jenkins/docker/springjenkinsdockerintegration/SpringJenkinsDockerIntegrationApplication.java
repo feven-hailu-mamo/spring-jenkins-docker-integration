3d7bb42de6edb340379fe70c9c827048bd900575
Feven <feven.h.mamo@gmail.com>
2020-05-05 15:06:33 -0500

    rest end point added

--- src/main/java/com/spring/jenkins/docker/springjenkinsdockerintegration/SpringJenkinsDockerIntegrationApplication.java
+++ src/main/java/com/spring/jenkins/docker/springjenkinsdockerintegration/SpringJenkinsDockerIntegrationApplication.java
@@ -2,10 +2,17 @@ package com.spring.jenkins.docker.springjenkinsdockerintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsDockerIntegrationApplication {
 
+    @GetMapping("/home")
+    public String home(){
+        return "The code is working...great";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringJenkinsDockerIntegrationApplication.class, args);
     }
