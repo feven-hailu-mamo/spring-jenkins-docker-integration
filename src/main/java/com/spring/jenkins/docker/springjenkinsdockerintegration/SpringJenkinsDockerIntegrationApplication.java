1834cbdf555bd19a06c5e40f709a0db91d0b5ec7
Feven <feven.h.mamo@gmail.com>
2020-05-05 15:13:39 -0500

    Logger added

--- src/main/java/com/spring/jenkins/docker/springjenkinsdockerintegration/SpringJenkinsDockerIntegrationApplication.java
+++ src/main/java/com/spring/jenkins/docker/springjenkinsdockerintegration/SpringJenkinsDockerIntegrationApplication.java
@@ -1,5 +1,7 @@
 package com.spring.jenkins.docker.springjenkinsdockerintegration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringJenkinsDockerIntegrationApplication {
 
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsDockerIntegrationApplication.class);
+
     @GetMapping("/home")
     public String home(){
         return "The code is working...great";
     }
     public static void main(String[] args) {
+        logger.info("Application executed...");
         SpringApplication.run(SpringJenkinsDockerIntegrationApplication.class, args);
     }
 
